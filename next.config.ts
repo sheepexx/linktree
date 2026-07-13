c374501f7d0f9af8a32ff14bbb2784f913e2c824
sheepex_ <148464281+sheepexx@users.noreply.github.com>
2026-07-13 16:14:41 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
